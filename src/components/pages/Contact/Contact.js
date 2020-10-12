efd82723d1aaca6271d3bc0da0fc2cf913f67ef1
pachkovska <pachkovska@gmail.com>
2020-10-11 22:18:57 -0700

    minor fixes

--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -4,14 +4,16 @@ import { Button, Form, FormGroup, Input} from 'reactstrap';
 import ContactImage from '../../../images/contact-page-img.jpg';
 
 export default function Contact() {
+
     const formRef = useRef();
 
     const onFormSend = ev => {
+        ev.preventDefault();
         emailjs.sendForm(process.env.REACT_APP_EMAILJS_SERVICE_ID,
                          process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
                          ev.target,
                          process.env.REACT_APP_EMAILJS_USER_ID)
-            .then(() => {
+            .then((res) => {
                 formRef.current.reset();
             }, (error) => {
                 console.log(error);
@@ -19,7 +21,7 @@ export default function Contact() {
     }
 
     return (
-        <div className="contact-page">
+        <div className="contact-page grid-left">
             <img className="contact-image" src={ContactImage} alt="Contact Me"/>
 
             <Form className="contact-form" innerRef={formRef}  onSubmit={onFormSend}>
